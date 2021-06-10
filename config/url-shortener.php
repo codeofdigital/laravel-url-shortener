6ea79de977182fefcc8540953aac846fb250c13d
bryanadamloh97 <bryanloh@talentcloud.ai>
2021-06-10 13:45:47 +0800

    Change config:

--- config/url-shortener.php
+++ config/url-shortener.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    'default' => env('URL_SHORTENER_DRIVER', 'bit.ly'),
+    'default' => env('URL_SHORTENER_DRIVER', 'bit_ly'),
 
     'shorteners' => [
-        'bit.ly' => [
+        'bit_ly' => [
             'driver' => 'bit_ly',
             'domain' => env('URL_SHORTENER_PREFIX', 'bit.ly'),
             'token' => env('URL_SHORTENER_API_TOKEN'),
